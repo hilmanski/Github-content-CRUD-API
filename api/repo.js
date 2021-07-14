eaeb53f9d69cc8edc3e5f9a7241ed30f71c88734
hilmanski <hilman@hilman.com>
2021-07-14 13:41:51 +0800

    change cors rules

--- api/repo.js
+++ api/repo.js
@@ -16,7 +16,11 @@ const octokit = new Octokit({
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true)
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
 
     if(req.method === 'GET') {
         //Single Content
